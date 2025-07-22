e465e7f33d852e3fc367b1b18f8f0ade86506d4d
Dramur <agskillup@gmail.com>
2025-07-22 13:20:45 +0100

    Update (description and other)

--- bot/factories.py
+++ bot/factories.py
@@ -1,47 +1,68 @@
 # Factories.py является центральным "диспетчером" команд
 
+from typing import Optional, Dict
+from bot.base import BotCommand  # Импортируем базовый класс для type hinting
 from bot.commands.help_menu import HelpMenuCommand
-# from bot.commands.command_dice import DiceCommand
-# from bot.commands.command_notebook import NotebookCommand
 from bot.commands.command_currency import CurrencyCommand
-# from bot.commands.command_iam import IAmCommand
-# from bot.commands.command_temperature import TemperatureCommand
-# from bot.commands.command_some import SomeCommand
 from bot.commands.shutdown import ShutdownCommand
-# from bot.commands import COMMAND_CLASSES
 from bot.commands.command_dev import DevCommand
-# from bot.commands.command_answer import AnswerCommand
 from bot.commands.command_currency1 import Currency1Command
 
 
 # Factory pattern: фабрика для створення команд за ключовим словом
 class CommandFactory:
-    # commands_map = COMMAND_CLASSES.copy()
-    # commands_map["/shutdown"] = ShutdownCommand
-    # commands_map["/help"] = HelpMenuCommand
-
+    """
+    Фабрика для управления экземплярами команд.
+    Использует словарь для хранения единственного экземпляра каждой команды (Singleton-like),
+    чтобы избежать их пересоздания при каждом вызове.
+    """
     # Явно определяем все команды в одном месте для ясности и удобства поддержки.
-    commands_map = {
+    command_map: Dict[str, type[BotCommand]] = {
         "/help": HelpMenuCommand,
         "/shutdown": ShutdownCommand,
-#        "/dice": DiceCommand,
-#        "/notebook": NotebookCommand,
         "/currency": CurrencyCommand,
-#        "/iam": IAmCommand,
-#        "/some": SomeCommand,
         "/dev": DevCommand,
-#        "/answer": AnswerCommand,
         "/currency1": Currency1Command,
     }
 
+    # 2. Словарь для хранения ЕДИНСТВЕННЫХ экземпляров команд.
+    # Он будет лениво заполняться при первом запросе команды.
+    command_instances: Dict[str, BotCommand] = {}
 
     @staticmethod
-    def create_command(command_name):
+    # def create_command(command_name):
+    def create_command(command_name: str) -> Optional[BotCommand]:
+        """
+        Возвращает экземпляр команды по ее имени.
+        Если экземпляр для этой команды еще не создан, он будет создан
+        и сохранен для последующего переиспользования.
+        """
         base_command = command_name.split(' ')[0]
-        CommandClass = CommandFactory.commands_map.get(base_command)
+        # CommandClass = CommandFactory.commands_map.get(base_command)
+        # if CommandClass:
+        #     return CommandClass()
+
+        # Проверяем, есть ли у нас уже готовый экземпляр.
+        if base_command in CommandFactory.command_instances:
+            return CommandFactory.command_instances[base_command]
+
+        # 4. Если экземпляра нет, ищем класс команды.
+        CommandClass = CommandFactory.command_map.get(base_command)
         if CommandClass:
-            return CommandClass()
+            # 5. Создаем экземпляр ОДИН РАЗ и сохраняем его.
+            instance = CommandClass()
+            CommandFactory.command_instances[base_command] = instance
+            return instance
+
         return None
+
+    @staticmethod
+    def get_available_commands() -> dict[str, BotCommand]:
+        """
+        Возвращает словарь всех доступных КЛАССОВ команд для использования, например, в /help.
+        Это позволяет команде /help самой решать, как отображать информацию (например, из docstring класса).
+        """
+        return CommandFactory.command_instances
 
 # # Преимущества этого подхода:
 # 1. Явное объявление: Все команды, включая `/currency` и `/currency1`, явно прописаны в `commands_map`.
