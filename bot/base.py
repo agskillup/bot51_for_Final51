730c04f09aedf1dd7990b9cc989636a04cfdee4c
Dramur <agskillup@gmail.com>
2025-07-22 11:23:11 +0100

    Update (add atributes) Liskov Substitution Principle

--- bot/base.py
+++ bot/base.py
@@ -1,13 +1,22 @@
+# Исправляем нарушение Принципа подстановки Барбары Лисков (Liskov Substitution Principle).
+# Простыми словами: дочерний класс не должен изменять данные родительского класса
+
 from abc import ABC, abstractmethod
+from typing import Optional
+
 
 # Command pattern: Абстрактний клас для всіх команд
 class BotCommand(ABC):
     @abstractmethod
-    def execute(self, text, chat_id, user_id):
+    # def execute(self, text, chat_id, user_id):
+
+    # добавлен атрибут `kwargs`, чтоб сделать класс более гибкой (- в файле унаследованный класс переопределяет его уже с другой сигнатурой: `execute(self, text, chat_id, user_id, **kwargs)` `role.py``RoleCommand`)
+    def execute(self, text: str, chat_id: int, user_id: int, **kwargs) -> Optional[str]:
         pass
 
 # Strategy pattern: Базовий клас для різних стратегій поведінки
 class CommandStrategy(ABC):
     @abstractmethod
-    def handle(self, text, chat_id, user_id):
+    # def handle(self, text, chat_id, user_id):
+    def handle(self, text: str, chat_id: int, user_id: int, **kwargs) -> Optional[str]:
         pass
